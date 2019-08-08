d4d4043cc60bd59b698a6c29f811670c97e67aa6
Patrick Flanagan <pflanagan01@protonmail.ch>
2019-08-08 15:16:24 +1000

    Add files via upload

---
+++ Cleanup Scripts/Display Customers Disk Usage.ps1
@@ -0,0 +1,19 @@
+﻿$_Creds=Get-Credential
+
+while($true){
+    $_Endpoint=Read-Host "Enter TA number: "
+
+    Foreach ($_Endpoint in $_Endpoint)
+    {
+        $_Disks = Get-WmiObject Win32_LogicalDisk -ComputerName $_Endpoint -Credential $_Creds -Filter DriveType=3 | 
+            Select-Object DeviceID, 
+                @{'Name'='Size'; 'Expression'={[math]::truncate($_.size / 1GB)}}, 
+                @{'Name'='Freespace'; 'Expression'={[math]::truncate($_.freespace / 1GB)}}
+
+        foreach ($_Disks in $_Disks)
+        {
+            $_Disks.DeviceID + $_Disks.FreeSpace.ToString("N0") + "GB / " + $_Disks.Size.ToString("N0") + "GB"
+
+         }
+     }
+}
