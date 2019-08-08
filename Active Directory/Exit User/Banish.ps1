996d11502ce68b906c1c8eed00c2dc703d74f05c
Patrick Flanagan <pflanagan01@protonmail.ch>
2019-08-08 15:38:24 +1000

    Updated from personal confluence page

--- Active Directory/Exit User/Banish.ps1
+++ Active Directory/Exit User/Banish.ps1
@@ -1,46 +1,35 @@
-﻿    #Import Active Directory Module
-Import-Module ActiveDirectory 
+#Import Active Directory Module
+Import-Module ActiveDirectory
 
+#Display warning
+Write-Host "WARNING: This script disables accounts on the specified domain, use with care" -ForegroundColor Yellow
 
-#Variables for Script
-$_Name=Read-Host "Enter account name you wish to disable"
+#Get standard variables
 $_Date=Get-Date -Format "MM/dd/yyyy"
+$_Server=Read-Host "Enter the domain you want to search"
 $_Creds=Get-Credential
 
-#Disable the Account by Input in $_Name Variable
-Disable-ADAccount -Identity $_Name -Credential $_Creds
-write-host "Disabling $_Name" -ForegroundColor Green
+while($true){
+
+#Requests user input username
+$_Name=Read-Host "Enter account name you wish to disable"
+
+#Lists the users AD groups and removes them
+Start-Sleep -s 10
+Get-ADUser -Identity $_Name -Properties MemberOf -Credential $_Creds| ForEach-Object {
+$_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Credential $_Creds -Confirm:$false
+} 
+write-host "User has been removed from the listed groups..." -ForegroundColor blue
+
+#Disables the account
+Disable-ADAccount -Identity $_Name -Server $_Server -Credential $_Creds
+write-host "Disabling $_Name..." -ForegroundColor blue
 
 #Sets the Description to Date the Script was ran and scrubs Office and Department to clear up Dynamic Distribution Lists
-Set-ADUser $_Name -Description "Disabled on $_Date" -Credential $_Creds
-write-host "Changing Description to Disabled on $_Date" -ForegroundColor Green
-
-#Removes All Group Membership except for Domain Users from User
-$group = get-adgroup "Domain Users"
-$groupSid = $group.sid
-$groupSid
-[int]$GroupID = $groupSid.Value.Substring($groupSid.Value.LastIndexOf("-")+1)
-Get-ADUser $_Name | Set-ADObject -Replace @{primaryGroupID="$GroupID"} -Credential $_Creds
-Remove-ADPrincipalGroupMembership -Identity $_Name -MemberOf $(Get-ADPrincipalGroupMembership -Identity $_Name -Credential $_Creds| Where-Object {$_.Name -ne "Domain Users"}) -Confirm:$false 
-Write-Host "Removing All Group Membership for $_Name" -ForegroundColor Green
-
-#Creates a variable that configures a remote session to our Exchange Servers to allow Exchange commands through powershell 
-$s=New-PSSession -ConfigurationName microsoft.exchange -ConnectionUri http://VSEMPPRDARC20/powershell  -Credential $_Creds -AllowRedirection 
-Write-Host "Creating New Exchange Session" -ForegroundColor Green
-
-#Imports the configured remote session
-Import-PSSession $s -DisableNameChecking
-Write-Host "Importing New Exchange Session" -ForegroundColor Green
-
-#Disables mailbox in exchange
-Disable-Mailbox -Identity $_Name
-Set-CASMailbox -Identity $_Name -ActiveSyncEnabled $false 
-write-host "Exchange Mailbox has been Disabled for $_Name" -ForegroundColor Green
-
-#closes the remote session
-Remove-PSSession $s  
-Write-Host "Closing Exchange Session" -ForegroundColor Green
-
+Set-ADUser $_Name -Server $_Server -Description "Disabled on $_Date" -Credential $_Creds
+write-host "Changing Description to Disabled on $_Date..." -ForegroundColor blue
 
 #Hard Stop for Script to keep window open
-Read-Host "User $_Name has been exited"
+Write-Host "User account $_Name has been disabled" -ForegroundColor Green
+Write-Host ""
+}
